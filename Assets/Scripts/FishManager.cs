5c9e165132a49cb73cc0095bbb05fafc2aaec843
AbelW <unoctium1@gmail.com>
2019-06-13 15:47:05 -0700

    Created wave scriptables for handling wave logic, and implemented wave logic

--- Assets/Scripts/FishManager.cs
+++ Assets/Scripts/FishManager.cs
@@ -1,28 +1,22 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 namespace FishBash
 {
     public class FishManager : MonoBehaviour
     {
-        public int fishToSpawn;
-        public int maxRadius;
-        public int minRadius;
-
         public static FishManager instance = null;
+
+        public Waves waveList;
+        public TextMeshProUGUI uiText;
+
+        int currWave = 0;
 
         [Range(1,10)]
         public float innerRadius;
 
-
-        // \todo - Custom editor window to clean this up
-        [Range(1, 10)]
-        public float speedMax;
-        [Range(1, 10)]
-        public float speedMin;
-
-        public GameObject[] fishPrefab;
         public GameObject platform;
 
         private IList<IFish> fishList;
@@ -43,14 +37,8 @@ namespace FishBash
         void Start()
         {
             fishList = new List<IFish>();
-            for(int i = 0; i < fishToSpawn; i++)
-            {
-                Vector2 position = Utility.RandomPointOnUnitCircle(maxRadius, minRadius);
-                IFish fish = SpawnFish(fishPrefab[UnityEngine.Random.Range(0,fishPrefab.Length)], position, UnityEngine.Random.Range(speedMin, speedMax));
-                fishList.Add(fish);
-            }
+            StartCoroutine(BeginGame());
         }
-
 
         /// <summary>
         /// Creates a fish of the specified object at the given position with the given speed
@@ -59,7 +47,7 @@ namespace FishBash
         /// <param name="position">Vector2 specifying x and z position of the fish</param>
         /// <param name="speed">Speed of the newly created fish</param>
         /// <returns>IFish component created</returns>
-        IFish SpawnFish(GameObject fishToSpawn, Vector2 position, float speed)
+        private IFish SpawnFish(GameObject fishToSpawn, Vector2 position, float speed)
         {
             
             GameObject fish = Instantiate(fishToSpawn, new Vector3(position.x, 0, position.y), new Quaternion(), transform);
@@ -68,6 +56,44 @@ namespace FishBash
             return toReturn;
         }
 
+        /// <summary>
+        /// Creates a fish from a external scriptable asset
+        /// </summary>
+        /// <param name="fishToSpawn">Scriptable fish to spawn in</param>
+        /// <param name="currentWave">Scriptable wave setting external parameters</param>
+        /// <returns></returns>
+        IFish SpawnFish(FishScriptable fishToSpawn, WaveScriptable currentWave)
+        {
+            Vector2 position;
+            float speed;
+
+            if (fishToSpawn.randomPosition) {
+                position = Utility.RandomPointOnUnitCircle(currentWave.maxRadius, currentWave.minRadius);
+            }
+            else
+            {
+                position = fishToSpawn.spawnPosition;
+            }
+
+            if (fishToSpawn.randomSpeed)
+            {
+                speed = UnityEngine.Random.Range(currentWave.speedMin, currentWave.speedMax);
+            }
+            else
+            {
+                speed = fishToSpawn.speed;
+            }
+
+            return SpawnFish(fishToSpawn.fishPrefab, position, speed);
+
+        }
+
+
+        /// <summary>
+        /// Destroys the given fish after a set time
+        /// </summary>
+        /// <param name="toDestroy">Fish object to destroy</param>
+        /// <param name="t">Time in seconds to wait before destroying</param>
         public void DestroyFish(IFish toDestroy, float t)
         {
             fishList.Remove(toDestroy);
@@ -75,10 +101,81 @@ namespace FishBash
         }
 
 
-        // Update is called once per frame
-        void Update()
+        /// <summary>
+        /// Central game loop - runs each wave until all waves have been executed
+        /// </summary>
+        /// <returns></returns>
+        IEnumerator BeginGame()
         {
+            while (currWave < waveList.waves.Length)
+            {
+                yield return Break(currWave);
+                yield return BeginWave(waveList.waves[currWave]);
+                Debug.Log("Wave Over");
+                currWave++;
+            }
+            yield return DisplayText("Game Over!", 3);
+        }
 
+
+        /// <summary>
+        /// Handles internal wave logic
+        /// </summary>
+        /// <param name="wave">Current wave</param>
+        /// <returns></returns>
+        IEnumerator BeginWave(WaveScriptable wave)
+        {
+            if (wave.randomFish)
+            {
+                //Spawns fish in random order
+                for(int i = 0; i < wave.fishCount; i++)
+                {
+                    IFish f = SpawnFish(wave.fishInWave[UnityEngine.Random.Range(0, wave.fishInWave.Length)], wave);
+                    fishList.Add(f);
+                    yield return new WaitForSeconds(wave.timeBetweenFish);
+                }
+                
+            }
+            else
+            {
+                //Spawns fish in specified order
+                foreach (char c in wave.order.ToCharArray()){
+                    if (char.IsDigit(c))
+                    {
+                        IFish f = SpawnFish(wave.fishInWave[(int) char.GetNumericValue(c)], wave);
+                        fishList.Add(f);
+                    }
+                    yield return new WaitForSeconds(wave.timeBetweenFish);
+                }
+            
+            }
+            yield return null;
+        }
+
+
+        /// <summary>
+        /// Filler coroutine to run before each wave
+        /// </summary>
+        /// <param name="nextWave">Name of next wave</param>
+        /// <returns></returns>
+        IEnumerator Break(int nextWave)
+        {
+            yield return DisplayText("Beginning wave " + (nextWave+1) + "...", 3);
+        }
+
+        /// <summary>
+        /// Displays given text on screen for a set period of time
+        /// </summary>
+        /// <param name="text">Text to display</param>
+        /// <param name="timeToDisplay">Time to display text</param>
+        /// <returns></returns>
+        IEnumerator DisplayText(string text, float timeToDisplay)
+        {
+            uiText.text = text;
+            uiText.gameObject.SetActive(true);
+            yield return new WaitForSeconds(timeToDisplay);
+            uiText.gameObject.SetActive(false);
+            yield return null;
         }
     }
 }
---
+++ Assets/Scripts/WaveScriptable.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FishBash
+{
+    [CreateAssetMenu(fileName = "Data", menuName = "Waves/List", order = 1)]
+    public class Waves : ScriptableObject
+    {
+        public WaveScriptable[] waves;
+    }
+
+    [CreateAssetMenu(fileName = "Wave", menuName = "Waves/New Wave", order = 2)]
+    public class WaveScriptable : ScriptableObject
+    {
+        public string waveName;
+        public FishScriptable[] fishInWave;
+
+        public float timeBetweenFish;
+        
+        public bool randomFish = true;
+        public int fishCount; //Only matters if randomFish is true, otherwise fishCount is the length of order
+
+        //order is a string dictating the indices of fish to spawn in fishInWave list;
+        public string order;
+
+        [Range(1, 10)]
+        public float speedMax;
+        [Range(1, 10)]
+        public float speedMin;
+        [Range(1, 75)]
+        public int maxRadius;
+        [Range(1, 75)]
+        public int minRadius;
+
+    }
+
+    [CreateAssetMenu(fileName = "Enemy", menuName = "Waves/New Enemy", order = 3)]
+    public class FishScriptable : ScriptableObject
+    {
+        public GameObject fishPrefab;
+
+        public bool randomSpeed = true;
+        public bool randomPosition = true;
+
+        public Vector2 spawnPosition;
+        public float speed;
+    }
+}
