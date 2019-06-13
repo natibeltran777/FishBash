0e444a25cc30c357e70cbb07fbe8b07fd98cb2c8
Beltran <wbeltran@ead.ubc.ca>
2019-06-13 11:21:17 -0700

    Fixed errors, physics is still a bit weird

--- Assets/Scripts/BatCapsuleFollower.cs
+++ Assets/Scripts/BatCapsuleFollower.cs
@@ -4,30 +4,38 @@ using UnityEngine;
 
 public class BatCapsuleFollower : MonoBehaviour
 {
-    private BatCapsule batFollower;
+    private BatCapsule batCapsuleToFollow;
     private Rigidbody rigidbody;
     private Vector3 velocity;
 
     [SerializeField]
     private float sensitivity = 100f;
 
+    /// <summary>
+    /// Initialize rigid body component of the BatFollower
+    /// </summary>
     private void Awake()
     {
         rigidbody = GetComponent<Rigidbody>();
     }
+    /// <summary>
+    /// Destination is the capsule on the bat. 
+    /// We get a velocity vector by subtracting the destination by the current position of the batfollower
+    /// So when we swing the bat, the follower follows right behind it at a certain velocity. 
+    /// This is what gives our swings power. 
+    /// </summary>
     private void FixedUpdate()
     {
-        Vector3 destination = batFollower.transform.position;
+        Vector3 destination = batCapsuleToFollow.transform.position;
         rigidbody.transform.rotation = transform.rotation;
-
         velocity = (destination - rigidbody.transform.position) * sensitivity;
 
         rigidbody.velocity = velocity;
-        transform.rotation = batFollower.transform.rotation;
+        transform.rotation = batCapsuleToFollow.transform.rotation;
     }
 
     public void SetFollowTarget(BatCapsule batFollower)
     {
-        this.batFollower = batFollower;
+        this.batCapsuleToFollow = batFollower;
     }
 }
