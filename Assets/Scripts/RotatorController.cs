d0a67bb3ca2550dd56492f395a2a4822b7ae3a76
AbelW <unoctium1@gmail.com>
2019-07-04 17:26:42 -0700

    Made simulator

---
+++ Assets/Scripts/RotatorController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FishBash
+{
+    public class RotatorController : MonoBehaviour
+    {
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                transform.Rotate(0, -5f, 0);
+            }
+            if(Input.GetKey(KeyCode.RightArrow))
+            {
+                transform.Rotate(0, 5f, 0);
+            }
+        }
+    }
+}
