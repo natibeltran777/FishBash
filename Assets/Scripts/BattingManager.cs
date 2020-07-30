53e15a9f6764cead105fa90f720724d2b855d8b4
abelw <unoctium1@gmail.com>
2020-07-29 20:33:25 -0700

    Update fish, add new bat model, adjust sound

--- Assets/Scripts/BattingManager.cs
+++ Assets/Scripts/BattingManager.cs
@@ -7,17 +7,6 @@ namespace FishBash
 
     public class BattingManager : GameManager
     {
-        // Start is called before the first frame update
-        void Start()
-        {
-
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-
-        }
 
         #region COROUTINES
         protected override IEnumerator DetractLives()
