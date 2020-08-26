366f636ab719c374dc19045b9f9aa99756e8dfd5
wbeltran777 <williambeltran1997@gmail.com>
2020-08-25 19:16:16 -0700

    Added new target model

--- Assets/Scripts/SimpleTarget.cs
+++ Assets/Scripts/SimpleTarget.cs
@@ -8,12 +8,12 @@ public class SimpleTarget : TargetBehaviour
 {
     [SerializeField] private Transform m_target;
     private float m_glowTransitionDuration = 0.2f;
-    private float m_glowOnVal = 0.5f;
+    private float m_glowOnVal = 0.2f;
     private float m_glowOffVal = 20f;
     private float m_targetPickCoolDown = 2.5f;
     private bool m_isOn = true;
     private bool m_targetIsSelected = false;
-    private Material m_glowMaterial;
+    private Material[] m_glowMaterials;
     private static int glowId = Shader.PropertyToID("_GlowPower");
 
     public override Transform GetTargetMesh 
@@ -26,7 +26,7 @@ public class SimpleTarget : TargetBehaviour
 
     private void Start()
     {
-        m_glowMaterial = transform.parent.GetComponentInChildren<Renderer>().material;
+        m_glowMaterials = transform.parent.GetComponentInChildren<Renderer>().materials;
         OnTargetUngazed();
     }
 
@@ -34,9 +34,11 @@ public class SimpleTarget : TargetBehaviour
     {
         if (!m_isOn)
         {
-            
-            m_glowMaterial.DOFloat(m_glowOnVal, glowId, m_glowTransitionDuration);
-            m_isOn = true;
+            foreach(Material material in m_glowMaterials)
+            {
+                material.DOFloat(m_glowOnVal, glowId, m_glowTransitionDuration);
+                m_isOn = true;
+            }
         }
     }
 
@@ -44,8 +46,11 @@ public class SimpleTarget : TargetBehaviour
     {
         if (m_isOn)
         {
-            m_glowMaterial.DOFloat(m_glowOffVal, glowId, m_glowTransitionDuration);
-            m_isOn = false;
+            foreach (Material material in m_glowMaterials)
+            {
+                material.DOFloat(m_glowOffVal, glowId, m_glowTransitionDuration);
+                m_isOn = false;
+            }
         }
     }
     
